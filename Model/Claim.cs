17042c9edee6ea7dc9fcd4567d5fcfcfde247849
Tomas Guardo <39739544+Tomig02@users.noreply.github.com>
2025-11-01 21:34:09 -0300

    new data-holding classes for car claim data

---
+++ Model/Claim.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+
+namespace app_reclamos_seguros.Model
+{
+    public abstract class Claim
+    {
+        // claim data
+        public int ClaimNumber { get; private set; }
+        public string Description { get; private set; }
+        public string Direction { get; private set; }
+        public string City { get; private set; }
+        public DateTime DateAndHour { get; private set; }
+
+        // client data
+        public int ClientDNI { get; private set; }
+        public string ClientName { get; private set; }
+        public string ClientSurname { get; private set; }
+        public int PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+
+        // policy data
+        public int PolicyNumber { get; private set; }
+        public string CompanyName { get; private set; }
+        public string Coverage  { get; private set; }
+
+
+        public Claim(string jsonString) 
+        {
+            JObject token = JObject.Parse(jsonString);
+
+            this.ClaimNumber = (int) token.SelectToken("claim_number");
+            this.Description = (string) token.SelectToken("description");
+            this.Direction = (string) token.SelectToken("direction");
+            this.City = (string) token.SelectToken("city");
+            this.DateAndHour = (DateTime) token.SelectToken("date_and_hour");
+            this.ClientDNI = (int) token.SelectToken("dni");
+            this.ClientName = (string) token.SelectToken("name");
+            this.ClientSurname = (string) token.SelectToken("surname");
+            this.PhoneNumber = (int) token.SelectToken("phone_number");
+            this.Email = (string) token.SelectToken("email");
+            this.PolicyNumber = (int) token.SelectToken("policy_number");
+            this.CompanyName = (string) token.SelectToken("company");
+            this.Coverage = (string) token.SelectToken("coverage");
+        }
+
+        public Claim(int claimNumber, string description, string direction, string city, DateTime dateAndHour, 
+            int clientDNI, string clientName, string clientSurname, int phoneNumber, string email, int policyNumber, 
+            string companyName, string coverage)
+        {
+            this.ClaimNumber = claimNumber;
+            this.Description = description;
+            this.Direction = direction;
+            this.City = city;
+            this.DateAndHour = dateAndHour;
+            this.ClientDNI = clientDNI;
+            this.ClientName = clientName;
+            this.ClientSurname = clientSurname;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+            this.PolicyNumber = policyNumber;
+            this.CompanyName = companyName;
+            this.Coverage = coverage;
+        }
+    }
+}
---
+++ Model/VehicleClaim.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace app_reclamos_seguros.Model
+{
+    public class VehicleClaim : Claim
+    {
+        // car data
+        public string vehicleBrand;
+        public string vehicleModel;
+        public string licensePlate;
+        public string registeredOwner;
+
+        public VehicleClaim(string jsonString) : base(jsonString) {
+            JObject token = JObject.Parse(jsonString);
+
+            this.vehicleBrand = (string) token.SelectToken("brand");
+            this.vehicleModel = (string) token.SelectToken("model");
+            this.licensePlate = (string) token.SelectToken("license_plate");
+            this.registeredOwner = (string) token.SelectToken("registered_owner");
+        }
+
+        public VehicleClaim(int claimNumber, string description, string direction, string city, DateTime dateAndHour,
+            int clientDNI, string clientName, string clientSurname, int phoneNumber, string email, int policyNumber,
+            string companyName, string coverage, string vehicleBrand, string vehicleModel, string licensePlate, 
+            string registeredOwner) 
+            : base(claimNumber, description, direction, city, dateAndHour,
+            clientDNI, clientName, clientSurname, phoneNumber, email, policyNumber,
+            companyName, coverage)
+        {
+            this.vehicleBrand = vehicleBrand;
+            this.vehicleModel = vehicleModel;
+            this.licensePlate = licensePlate;
+            this.registeredOwner = registeredOwner;
+        }
+    }
+}
